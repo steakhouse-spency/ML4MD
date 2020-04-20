d193c31fce4f7dbed1b2cc10cf640b8a0a5d833b
spencer-ortega <sportega@usc.edu>
2020-04-20 11:40:59 -0700

    worked on script to give us the correct anchors to hold tube down during sim

---
+++ spencer/sim/get_nonanchors.py
@@ -0,0 +1,96 @@
+
+import os
+import sys
+import subprocess
+
+label=sys.argv[1]
+
+material = "C"
+
+def getAtomData():
+
+	fpdb = open("%s.pdb" % (label), "r")
+	tube_z = []
+	water_id = [] 
+	for line in fpdb:
+		cols = line.split()
+		if cols[0] == "ATOM":
+			if cols[2] == material:
+				tube_z.append(float(cols[7]))
+			elif cols[2] == "MOL":
+				water_id.append(cols[1])
+			else:
+				print("error")
+				print(">%s<" % (cols[2]))
+				exit(1)
+
+	return tube_z, water_id
+
+def Diff(li1, li2): 
+    return (list(set(li1) - set(li2))) 
+
+def get_nonanchors(tube_z):
+
+	# number of tube atoms
+	tube_len = len(tube_z)
+
+	# get sorted indicies
+	sorted_i = [i[0] for i in sorted(enumerate(tube_z), key=lambda x:x[1])]
+
+	# where to place the ring
+	rings=[0.25, 0.5, 0.75, 1.0]
+
+	# atoms per ring
+	ring_len = 10
+	half = int(ring_len/2)
+
+	# for every ring
+	start = ring_len
+	rng = [None, None]
+	non_anchor_i = []
+	for ring in rings:
+
+		# get id of atom in middle of ring
+		mid = int(tube_len*ring)
+		
+		# get range of atoms in ring
+		# using 10 atoms/ring 
+		rng[0] = mid - (half - 1)
+		rng[1] = mid + half
+
+		# append non-anchor atom ids
+		if ring != 1.0:
+			# from 'start' to the first atom in this ring
+			end = rng[0]
+		else:
+			# from end of last ring to end of tube
+			# -10 for last ring at end of tube
+			#echo "last iter"
+			end = tube_len - ring_len
+
+		# append new range of atom ids
+		non_anchor_i += [*range(start, end)]
+
+		# update start to the last atom in this ring + 1
+		start = rng[1] + 1
+
+	# convert indicies to atom ids
+	non_anchor_id = []
+	for i, anchor_i in enumerate(non_anchor_i):
+		non_anchor_id.append(sorted_i[anchor_i] + 1)
+		# sorted_i[i] += 1
+
+	# sort and return ids
+	non_anchor_id.sort()
+
+	return non_anchor_id
+
+tube_z, water_id = getAtomData()
+tube_id = get_nonanchors(tube_z)
+
+print(len(tube_id))
+print(len(water_id))
+for i in tube_id:
+	print(i)
+
+
--- spencer/sim/runtherm.sh
+++ spencer/sim/runtherm.sh
@@ -46,73 +46,80 @@ for file in ${datafiles[*]}; do
 	cp $wd/input_file/$material/CH.rebo .
 
 
-	# get count of atoms for both nanotube and water
-	# format: (count_1, id_1, ... , count_n, id_n)
-	atom_count=($(cat $label.pdb | awk '{if($1=="ATOM"){print $3}}' | sort | uniq -c ))
+	# # get count of atoms for both nanotube and water
+	# # format: (count_1, id_1, ... , count_n, id_n)
+	# atom_count=($(cat $label.pdb | awk '{if($1=="ATOM"){print $3}}' | sort | uniq -c ))
 
-	# for every other index
-	nums=($(seq 1 2 ${#atom_count[*]}))
-	for i in ${nums[*]}; do
-		# get values
-		count=${atom_count[$((i-1))]}
-		id=${atom_count[$((i))]}
+	# # for every other index
+	# nums=($(seq 1 2 ${#atom_count[*]}))
+	# for i in ${nums[*]}; do
+	# 	# get values
+	# 	count=${atom_count[$((i-1))]}
+	# 	id=${atom_count[$((i))]}
 		
-		# store counts
-		if [ $id == $material ]; then
-			tube_count=$count
-		else
-			water_count=$count
-		fi
-	done
+	# 	# store counts
+	# 	if [ $id == $material ]; then
+	# 		tube_count=$count
+	# 	else
+	# 		water_count=$count
+	# 	fi
+	# done
 
 
-	echo "tube:" $tube_count
-	echo "water:" $water_count
+	# echo "tube:" $tube_count
+	# echo "water:" $water_count
 
-	# percentages represent where rings will be placed
-	# no ring at 1.0
-	rings=(0.25 0.5 0.75 1.0)
+	# # percentages represent where rings will be placed
+	# # no ring at 1.0
+	# rings=(0.25 0.5 0.75 1.0)
 
-	# atoms per ring
-	ring_len=10
+	# # atoms per ring
+	# ring_len=10
 
-	# store all non anchor ids
-	non_anchor=( )
+	# # store all non anchor ids
+	# non_anchor=( )
 
-	# for every ring
-	start=$((ring_len + 1))
-	half_ring=$(echo "$ring_len * 0.5 / 1" | bc)
-	for ring in ${rings[*]}; do
-		# get id of atom in middle of ring
-		# bash cant do float arithmitic -__-
-		# divide by 1 to floor decimal value
-		mid=$(echo "$tube_count * $ring / 1" | bc)
+	# # for every ring
+	# start=$((ring_len + 1))
+	# half_ring=$(echo "$ring_len * 0.5 / 1" | bc)
+	# for ring in ${rings[*]}; do
+	# 	# get id of atom in middle of ring
+	# 	# bash cant do float arithmitic -__-
+	# 	# divide by 1 to floor decimal value
+	# 	mid=$(echo "$tube_count * $ring / 1" | bc)
 		
-		# get range of atoms in ring
-		# using 10 atoms/ring 
-		range[0]=$(echo "$mid - $half_ring - 1" | bc)
-		range[1]=$((mid + half_ring))
+	# 	# get range of atoms in ring
+	# 	# using 10 atoms/ring 
+	# 	range[0]=$(echo "$mid - $half_ring - 1" | bc)
+	# 	range[1]=$((mid + half_ring))
 
-		# debug
-		echo ring middle: $mid
-		echo ring range: ${range[*]}
+	# 	# debug
+	# 	echo ring middle: $mid
+	# 	echo ring range: ${range[*]}
 	
-		# append non-anchor atom ids
-		if [ "$ring" == "1.0" ]; then
-			# from end of last ring to end of tube
-			# -10 for last ring at end of tube
-			#echo "last iter"
-			end=$((tube_count - 10))
-		else
-			# from 'start' to the first atom in this ring
-			end=$((range[0] - 1))
-		fi
-		non_anchor+=($(seq $start $end))
-		echo non-anchors: [$start, $end]		
-		echo --------
-		# update start to the last atom in this ring + 1
-		start=$((range[1] + 1))
-	done
+	# 	# append non-anchor atom ids
+	# 	if [ "$ring" == "1.0" ]; then
+	# 		# from end of last ring to end of tube
+	# 		# -10 for last ring at end of tube
+	# 		#echo "last iter"
+	# 		end=$((tube_count - 10))
+	# 	else
+	# 		# from 'start' to the first atom in this ring
+	# 		end=$((range[0] - 1))
+	# 	fi
+	# 	non_anchor+=($(seq $start $end))
+	# 	echo non-anchors: [$start, $end]		
+	# 	echo --------
+	# 	# update start to the last atom in this ring + 1
+	# 	start=$((range[1] + 1))
+	# done
+
+
+	output=($(python3 $wd/sim/get_nonanchors.py $label))
+	water_count=${output[0]}
+	tube_count=${output[1]}
+	non_anchor=${output[2]}
+
 
 	# get the id of the first water atom (H/O)
 	water_start=$((tube_count + 1))
@@ -137,7 +144,7 @@ for file in ${datafiles[*]}; do
 	echo "${lmprun} < ${infile}" >> $slfile
 
 	# submit to slurm
-	sbatch $label-therm.slurm
+	# sbatch $label-therm.slurm
 	#sleep 2
 
 	# reset to wd
